4ae8fab9f675b7a4d7be44862f62229633090b4a
Chenchu-Parameswar-Kotari <parameswar.kotari@gmail.com>
2022-07-30 11:21:29 +0530

    intial starer prject

---
+++ spring-boot-sessions/src/main/java/com/riversoftit/SpringBootSessionsApplication.java
@@ -0,0 +1,13 @@
+package com.riversoftit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionsApplication.class, args);
+	}
+
+}

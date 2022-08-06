8a0acb6344c16984d246f310d4bff60a16ce38b0
ganilkumar13 <anilydp8989@gmail.com>
2022-08-06 10:51:08 +0530

    added hello world example#	modified:   bin/.gitignore

---
+++ core-java-sessions/src/Sort.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Scanner;
+
+public class Sort {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//int []m=new int[10];
+		System.out.print("enter no of elemrnts in array: ");
+		int n=sc.nextInt();
+		int []m=new int[n];
+		for(int i=0;i<n;i++) {
+			m[i]=sc.nextInt();
+		}
+		for(int k=0;k<m.length;k++) {
+			for(int t=k+1;t<m.length;t++) {
+				int temp=0;
+				if(m[k]>m[t]) {
+					temp=m[k];
+					m[k]=m[t];
+					m[t]=temp;
+				}
+			}
+			System.out.println("the sorted elements are :"+m[k]);
+		}
+		
+	}
+
+}
---
+++ core-java-sessions/src/com/riversoftit/collections/Arraylistanil.java
@@ -0,0 +1,10 @@
+package com.riversoftit.collections;
+
+public class Arraylistanil {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}

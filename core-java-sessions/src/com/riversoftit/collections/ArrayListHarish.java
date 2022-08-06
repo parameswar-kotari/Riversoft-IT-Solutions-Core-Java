363694d5fa4cf0712031ccec576632dc9af4ac82
Haish3574 <pinniharish7042@gmail.com>
2022-08-06 10:45:37 +0530

    new class is added

---
+++ core-java-sessions/src/com/riversoftit/collections/ArrayListHarish.java
@@ -0,0 +1,12 @@
+package com.riversoftit.collections;
+
+public class ArrayListHarish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
+	}
+
+}

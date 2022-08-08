3983a6a37a15e8d9512e4353c574bb88058c1eaa
ganilkumar13 <anilydp8989@gmail.com>
2022-08-08 12:50:36 +0530

    addeed soting anil

---
+++ core-java-sessions/src/com/riversoftit/collections/Usort.java
@@ -0,0 +1,15 @@
+package com.riversoftit.collections;
+
+import java.util.Arrays;
+
+public class Usort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []m= {12,32,34,21,45,76,47};
+		Arrays.sort(m);
+		for(int i=0;i<m.length;i++) {
+		System.out.println(m[i]);
+		}
+  }
+}

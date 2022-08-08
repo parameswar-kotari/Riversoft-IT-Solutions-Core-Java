c022efc4478b3b3445b225c944e150afbf57dc14
pooja4800 <poojam4800@gmail.com>
2022-08-08 13:08:02 +0530

    added new class

--- core-java-sessions/src/com/riversoftit/collections/Pooja_arraylist.java
+++ core-java-sessions/src/com/riversoftit/collections/Pooja_arraylist.java
@@ -1,12 +1,20 @@
 package com.riversoftit.collections;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pooja_arraylist {
 	
 public static  void main(String[]args)
 {
-
-System.out.println("hello world");
+	List<String> list=new ArrayList<>() {};
+	list.add("sandhya");
+	list.add("lavanyaa");
+	list.add("syamala");
+	list.add("amala");
+	System.out.println(list.contains("pooja"));
+	
+			
 
 }
 }

a4d181d56cb05c77afa92b9425d89eddfe8a0f58
ganilkumar13 <110652426+ganilkumar13@users.noreply.github.com>
2022-08-09 12:30:53 +0530

    Add files via upload

---
+++ core-java-sessions/src/com/riversoftit/collections/LinkedList.java
@@ -0,0 +1,22 @@
+package Advanced;
+import java.util.*;
+
+public class Linke {
+    public static void main(String[] args) {
+		  
+    	//@SuppressWarnings("unchecked")
+		List<String> l= new LinkedList<>();
+		l.add("mahesh");
+
+				
+				
+				
+	    
+
+     
+       
+
+	}
+
+	
+}

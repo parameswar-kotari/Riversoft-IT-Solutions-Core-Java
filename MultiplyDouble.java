ebf502526edd718d760922310042e89d346d75f0
swathiyadav16 <110883763+swathiyadav16@users.noreply.github.com>
2022-08-10 20:27:40 +0530

    Add files via upload
    multiply of numbers using double data type

---
+++ MultiplyDouble.java
@@ -0,0 +1,11 @@
+public class MultiplyDouble {
+   public static void main(String[] args) {
+       double x = 15.7;
+       double y = 2.1;
+       int a = 3;
+       double z = x*y;
+       double b = a*x;
+       System.out.println(x + "*" + y + " = " + z);
+       System.out.println(a + "*" + x + " = " + b);
+   }
+}

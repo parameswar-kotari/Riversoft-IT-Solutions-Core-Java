b45e6484e0f9a08a55c8f244a8031439445f183f
bnharish12 <110820185+bnharish12@users.noreply.github.com>
2022-08-10 21:07:07 +0530

    Add files via upload

---
+++ BasicArithmetic.java
@@ -0,0 +1,23 @@
+package basicarithmetic;
+
+public class BasicArithmetic {
+	
+	public static void main(String args[]) {
+		int a=10;
+		int b=5;
+		double c=10.5;
+		double d=5.2;
+		float e=10.45f;
+		float f=10.33f;
+		double add=c+d;
+		float sub=e-f;
+		int multiply=a*b;
+		int division=a/b;
+		System.out.println("Add=" +add);
+		System.out.println("Sub=" +sub);
+		System.out.println("Multiply=" +multiply);
+		System.out.println("Division=" +division);
+		
+	}
+
+}

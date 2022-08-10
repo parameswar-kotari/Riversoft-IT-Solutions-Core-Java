91fc042efa5bdd5618b97c09a2919621c5174711
swathiyadav16 <110883763+swathiyadav16@users.noreply.github.com>
2022-08-10 20:19:52 +0530

    Add files via upload
    program using float data type

---
+++ FloatDataTypeExample.java
@@ -0,0 +1,9 @@
+public class FloatDataTypeExample  
+{  
+public static void main(String args[])   
+{  
+float x = 1.0f, y = 3.0f;   
+float z = x / y;            
+System.out.println("x/y = " + z); 
+}  
+}

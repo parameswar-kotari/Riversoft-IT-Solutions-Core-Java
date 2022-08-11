b6c1e5905933029dddf991653ee5f2fc5c9b29b9
Bhavishya-vontimitta <bhavishyaravi24@gmail.com>
2022-08-11 10:50:57 +0530

    addedVariables

--- core-java-sessions/src/com/riversoftit/collections/Bhavishya_arraylist.java
+++ core-java-sessions/src/com/riversoftit/collections/Bhavishya_arraylist.java
@@ -3,6 +3,16 @@ import java.util.*;
 
 public class Bhavishya_arraylist {
 	public static void main(String[]args) {
-System.out.println("hello world ");
+    int b=5;
+    int t = 7;
+    int Sum = b+t;
+    system.out.println(Sum);
+    
+    		
+    		
+    		
+    		
+    		
+    		
 }
 }

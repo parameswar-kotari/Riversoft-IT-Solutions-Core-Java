a51b925a7e0031e7773595ed4478c045258b9bcc
swathiyadav16 <110883763+swathiyadav16@users.noreply.github.com>
2022-08-11 17:46:40 +0530

    Add files via upload
    program using escape codes

---
+++ class1.java
@@ -0,0 +1,69 @@
+class class1
+
+{
+
+	public static void main(String args[])
+
+	{
+
+              
+
+		System.out.printf("Hello\b World\n");
+
+ 		
+
+        
+          
+
+		System.out.printf("Hello\nWorld\n");
+
+		
+
+    
+           
+
+		System.out.printf("Hello\t World\n"); 
+
+		
+
+		System.out.printf("Hello World \rDevil\n"); 
+
+		
+
+
+		System.out.printf("\"Decode School\"\n");
+
+
+
+		
+		System.out.printf("a\\b\n");
+
+                
+
+                
+
+		System.out.printf("%.2f\n",10/3.0); 
+
+		
+
+		
+		System.out.printf("%x\n",10); 
+
+		
+
+		
+
+		System.out.printf("%o\n",10); 
+
+		
+
+		
+		System.out.printf("%b\n",10); 
+
+		
+
+		
+
+	}
+
+}

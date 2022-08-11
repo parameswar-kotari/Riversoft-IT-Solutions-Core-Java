090bf8597bd8da3dd6f1c8f5263f157a00d2913a
bnharish12 <110820185+bnharish12@users.noreply.github.com>
2022-08-11 22:57:48 +0530

    Add files via upload

---
+++ core-java-sessions/src/com/riversoftit/collections/EscapeCodes.java
@@ -0,0 +1,14 @@
+package escapecodes;
+
+public class EscapeCodes {
+
+	public static void main(String[] args) {
+		
+		System.out.print("\t  Huddle Rise \n");
+		System.out.print("\t \'Near Biryani's\' \n");
+		System.out.print("\t \"Marathalli\" \n");
+		System.out.println("\t \\Bengaluru\\ \n");
+		
+	}
+
+}

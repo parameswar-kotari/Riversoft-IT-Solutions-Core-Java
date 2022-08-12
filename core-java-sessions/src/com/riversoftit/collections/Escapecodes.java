79ddd284407a73c04060fae79ac92e9e6a416102
pooja4800 <poojam4800@gmail.com>
2022-08-12 09:26:37 +0530

    add new class

---
+++ core-java-sessions/src/com/riversoftit/collections/Escapecodes.java
@@ -0,0 +1,8 @@
+package com.riversoftit.collections;
+
+public class Escapecodes {
+	public static void main(String[]args) {
+		System.out.println("Good morning! \n how are u all?");
+		
+	}
+ }
--- core-java-sessions/src/com/riversoftit/collections/VectorList.java
+++ core-java-sessions/src/com/riversoftit/collections/VectorList.java
@@ -1,16 +1,10 @@
 package com.riversoftit.collections;
 import java.util.Vector;
 
-public class VectorList {
+public class{
 	public static void main(String[]args) {
-		Vector<String>lists=new Vector<>();
-		//Using the add()method
-		lists.add("pooja");
-		lists.add("syamala");
-		//Using index number
-		lists.add("sandhya");
-		lists.add("lavanya");
-		System.out.println("Vector:" +lists);
+		
+	
 	}
 }
 

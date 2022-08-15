e003003fdabf20f2d9df1f2b40d397fb9a29086e
Venkatsai19hm <110887633+Venkatsai19hm@users.noreply.github.com>
2022-08-15 23:17:51 +0530

    Add files via upload
    conditional statements assignment

---
+++ SwitchCaseExample1.java
@@ -0,0 +1,28 @@
+package add;
+
+public class SwitchCaseExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		switch(x) {
+		case 1:System.out.println("outstanding");
+		break;
+		case 2:System.out.println("excellent");
+		break;
+		case 3:System.out.println("good");
+		break;
+		case 4:System.out.println("average");
+		break;
+		case 5:System.out.println("satisfactory");
+		break;
+		case 6:System.out.println("fail");
+		break;
+		default:System.out.println("bad");
+		}
+				}
+		
+
+	}
+
+
---
+++ VowelConsonant.java
@@ -0,0 +1,18 @@
+package add;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='z';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+			System.out.println("vowel");
+		}
+		else if(ch=='z')
+			
+		{
+			System.out.println("consonant");
+		}
+	}
+}

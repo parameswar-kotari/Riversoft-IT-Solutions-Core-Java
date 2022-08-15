7be1b0b89df9edc2d2e22710528d0f7cd9ab4675
pooja4800 <poojam4800@gmail.com>
2022-08-16 00:19:47 +0530

    reader

---
+++ core-java-sessions/src/com/riversoftit/collections/Evevandadd.java
@@ -0,0 +1,15 @@
+package com.riversoftit.collections;
+import java.util.*;
+
+public class Evevandadd {
+	public static void main(String[]args) {
+     Scanner cr = new Scanner(System.in);
+     System.out.println("Enter a number:");
+      int num = cr.nextInt();
+      
+      if (num %2 == 0)
+    	  System.out.println( "number is even");
+      else
+    	  System.out.println("number is odd");
+	}
+}

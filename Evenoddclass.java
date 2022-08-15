4ec1a791b3086853efcef180cb4fb51f259f4e81
Srihari161 <110887029+Srihari161@users.noreply.github.com>
2022-08-15 23:20:20 +0530

    Add files via upload
    conditional statements assignment

---
+++ Evenoddclass.java
@@ -0,0 +1,23 @@
+package add;
+
+public class Evenoddclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=36;
+		if(x%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+			
+		}
+		
+	
+		
+
+	}
+
+}
---
+++ SwitchCaseExample.java
@@ -0,0 +1,24 @@
+package add;
+
+class SwitchCaseExample {
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int y=1;
+switch(y) {
+case 1:System.out.println("outstanding");
+break;
+case 2:System.out.println("excellent");
+break;
+case 3:System.out.println("good");
+break;
+case 4:System.out.println("average");
+break;
+case 5:System.out.println("satisfactory");
+break;
+case 6:System.out.println("fail");
+break;
+default:System.out.println("bad");
+}
+		}
+}
+

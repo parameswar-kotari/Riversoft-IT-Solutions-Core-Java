9c6460c7dee8066f1a93d629069d412a5750f54c
pooja4800 <poojam4800@gmail.com>
2022-08-16 15:06:54 +0530

    read the project

---
+++ core-java-sessions/src/com/riversoftit/collections/Helloworld.java
@@ -0,0 +1,6 @@
+package com.riversoftit.collections;
+
+public class Helloworld {
+	
+
+}
---
+++ core-java-sessions/src/com/riversoftit/collections/Vowelconstant.java
@@ -0,0 +1,19 @@
+package com.riversoftit.collections;
+
+import java.util.*;
+
+public class Vowelconstant {
+	
+	public static void main(String[]args) {
+		Scanner pvr=new Scanner(System.in);
+		System.out.println("Enter a character");
+		char c=((pvr.nextLine()).charAt(0));
+		if
+			(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+			System.out.println("enter the letter is vowel");
+		else
+			System.out.println("enter the letter is constant");
+
+	
+	}		
+}

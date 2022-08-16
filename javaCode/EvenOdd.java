4da92a530e2741be0d0e62163ccb1a7db5f77636
Suryaprakash41444 <111303183+Suryaprakash41444@users.noreply.github.com>
2022-08-16 11:31:08 +0530

    Day5Assignment

---
+++ javaCode/EvenOdd.java
@@ -0,0 +1,28 @@
+//write a program which takes the number from the user and check wheather the given number is odd or even using if condition
+package javapractise;
+
+public class EvenOdd {
+
+	public static void main(String[] args){
+		int a = 10;
+		if(a%2==0) {
+			System.out.println("Given number is even");
+		}
+		else {
+			System.out.println("Given Number is Odd");
+			}
+		int b = 11;
+		if(b%2==0) {
+			System.out.println("Given number is even");
+		}
+		else {
+			System.out.println("Given Number is Odd");
+			}
+			
+		
+	}
+
+}
+
+//write a program which takes the character from the user and check wheather the given number is vowel or consonant bu using if else-if
+
---
+++ javaCode/performance.java
@@ -0,0 +1,31 @@
+package javapractise;
+
+public class performance {
+
+	public static void main(String[] args) {
+		char ch = 'B';
+		switch(ch) {
+		case 'A':
+			System.out.println("OutStanding");
+			break;
+		case 'B':
+			System.out.println("Excellent");
+			break;
+		case 'C':
+			System.out.println("Good");
+			break;
+		case 'D':
+			System.out.println("Average");
+			break;
+		case 'E':
+			System.out.println("Satisfactory");
+			break;
+		case 'F':
+			System.out.println("Fail");
+		
+		}
+		
+		
+	}
+
+}

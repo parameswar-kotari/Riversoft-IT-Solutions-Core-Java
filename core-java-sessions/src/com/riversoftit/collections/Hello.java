f7b9533024df303b69c05bdc277b093e99a33aeb
143Amarnatha <reddyamarnath83@gmail.com>
2022-08-16 12:14:21 +0530

    print the programme

---
+++ core-java-sessions/src/com/riversoftit/collections/Hello.java
@@ -0,0 +1,8 @@
+package com.riversoftit.collections;
+
+public class Hello {
+	public static void main(String[]args) {
+		System.out.println("Hello world");
+	}
+
+}
---
+++ core-java-sessions/src/com/riversoftit/collections/Helloworld.java
@@ -0,0 +1,12 @@
+package com.riversoftit.collections;
+
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}

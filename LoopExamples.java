4dade124ce90fbf927b2714290442f9c33c88ba3
Suryaprakash41444 <111303183+Suryaprakash41444@users.noreply.github.com>
2022-08-16 20:34:23 +0530

    Day6 Assignment

---
+++ LoopExamples.java
@@ -0,0 +1,35 @@
+package javaCoding;
+
+public class LoopExamples {
+
+	public static void main(String[] args) {
+		
+		
+		//while loop
+//		int num = 10,i=1,sum = 0;
+//		while(i<=num) {
+//			sum += i;
+//			i++;
+//			
+//		}
+//		System.out.println(sum);
+		
+		//for loop
+//   	int sum=0;
+//		for(int i=1;i<=10;i++) {
+//			sum = sum+i;
+//		}
+//		System.out.println(sum);
+		
+	//Do-while loop
+		int sum=0,i=1;
+		do {
+			sum+=i;
+			i++;
+		}while(i<=10);
+		System.out.println(sum);
+		
+
+	}
+
+}

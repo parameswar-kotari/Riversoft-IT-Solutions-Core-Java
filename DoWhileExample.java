036f4565b6188be3ec68c60f38a15d33157e6133
Venkatsai19hm <110887633+Venkatsai19hm@users.noreply.github.com>
2022-08-17 07:41:52 +0530

    Add files via upload
    iterations programs

---
+++ DoWhileExample.java
@@ -0,0 +1,16 @@
+package com.iterations;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do {
+			System.out.print(i);
+			i++;
+			
+		}while(i<=10);
+
+	}
+
+}

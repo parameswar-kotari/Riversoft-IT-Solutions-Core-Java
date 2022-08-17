4ec7daef5c23f50a714583fc84b971dcec1bb7b9
Srihari161 <110887029+Srihari161@users.noreply.github.com>
2022-08-17 07:45:29 +0530

    Add files via upload
    Iterations programs like for, while, dowhile  (day 6) assignment

---
+++ ForExample.java
@@ -0,0 +1,14 @@
+package com.iterations;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		int b=1;
+		for( int i=1;i<=10;++i)
+			System.out.print(i);
+			
+			
+
+	}
+
+}
---
+++ WhileExample.java
@@ -0,0 +1,15 @@
+package com.iterations;
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=10) {
+			System.out.print(i);
+			i++;
+		}
+
+	}
+
+}

c43ce697f4269b679778d25c43f959f0b2a80187
pooja4800 <poojam4800@gmail.com>
2022-08-19 00:55:11 +0530

    add programme

---
+++ core-java-sessions/src/com/riversoftit/collections/ArrayClass.java
@@ -0,0 +1,14 @@
+package com.riversoftit.collections;
+
+public class ArrayClass {
+	public static void main(String[]arg) {
+		int[] numbers = {20,68,95,82,45};
+		int i=0;
+		while(i<numbers.length) {
+			System.out.printf("%s \n",numbers[i]);
+			i++;
+		}
+		
+	}
+
+}

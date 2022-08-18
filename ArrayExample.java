f3029f7c74ee4dde5f9453b5f4dfb12e719f15a7
subbareddy-dot <110714419+subbareddy-dot@users.noreply.github.com>
2022-08-18 16:59:56 +0530

    Add files via upload
    Array program assignment

---
+++ ArrayExample.java
@@ -0,0 +1,33 @@
+package com.riversoftit;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		int[] numbers={48,25,92};
+		int i=0;
+		while(i<numbers.length) {
+			System.out.printf("%s \n", numbers[i]);
+			i++;
+		}
+	}
+}
+//		
+//		System.out.println("numbers="+numbers);
+//		
+//		
+//		
+		
+		
+		
+		
+		
+		
+			
+		
+
+		
+
+	
+
+

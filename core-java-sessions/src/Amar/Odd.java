dfd8d9ae9216ecff5fa3449d3207405dc1e5aa10
143Amarnatha <110604875+143Amarnatha@users.noreply.github.com>
2022-08-23 09:45:17 +0530

    odd or even program

---
+++ core-java-sessions/src/Amar/Odd.java
@@ -0,0 +1,23 @@
+package Amar;
+
+import java.util.Scanner;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		if(num%2==0) {
+			System.out.println(" is even");
+		}else {
+				System.out.println(" is odd");
+				
+			}
+		}
+		
+
+	}
+
+

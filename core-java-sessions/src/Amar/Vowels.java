9560eb051bd0c805a75e1375952c086decc17b40
143Amarnatha <110604875+143Amarnatha@users.noreply.github.com>
2022-08-23 10:02:46 +0530

    vowels program

---
+++ core-java-sessions/src/Amar/Vowels.java
@@ -0,0 +1,18 @@
+package Amar;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='a';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+		
+			System.out.println("is vowel");
+		}else {
+			System.out.println("is constant");
+			
+		}
+
+	}
+
+}

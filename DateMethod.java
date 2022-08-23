07d144a8ab9ccafd46a2a9e7034968bbfb5b6cef
bnharish12 <110820185+bnharish12@users.noreply.github.com>
2022-08-23 13:52:17 +0530

    Add files via upload

---
+++ DateMethod.java
@@ -0,0 +1,29 @@
+package date;
+import java.util.Date;
+public class DateMethod {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date date=new Date();
+		System.out.println(date.getDate());
+		System.out.println(date.getDay());
+		System.out.println(date.getHours());
+		System.out.println(date.getMonth());
+		System.out.println(date.getMinutes());
+		System.out.println(date.getSeconds());
+		System.out.println(date.getTime());
+		System.out.println(date.toInstant());
+		System.out.println(date.getYear());
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}

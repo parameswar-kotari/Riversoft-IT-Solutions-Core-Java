2be82b870dff8fa08ac66cb53994b6080b510295
subbareddy-dot <110714419+subbareddy-dot@users.noreply.github.com>
2022-08-23 16:23:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Student {
+	String subbareddy;
+	String ongole;
+	String amarnath;
+	int id ;
+	
+	
+	
+
+	public Student getSubbareddy() {
+		 String subbareddy;
+		return null;
+	}
+
+
+
+	public void setSubbareddy(String subbareddy) {
+		this.subbareddy = subbareddy;
+	}
+
+
+
+	public String getOngole() {
+		return ongole;
+	}
+
+
+
+	public void setOngole(String ongole) {
+		this.ongole = ongole;
+	}
+
+
+
+	public String getAmarnath() {
+		return amarnath;
+	}
+
+
+
+	public void setAmarnath(String amarnath) {
+		this.amarnath = amarnath;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+
+	@Override
+	public String toString() {
+		return "Student [subbareddy=" + subbareddy + ", ongole=" + ongole + ", amarnath=" + amarnath + ", id=" + id
+				+ ", getSubbareddy()=" + getSubbareddy() + ", getOngole()=" + getOngole() + ", getAmarnath()="
+				+ getAmarnath() + ", getId()=" + getId() + "]";
+	}
+
+}
---
+++ Subbu.java
@@ -0,0 +1,20 @@
+
+public class Subbu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student();
+		s1.ongole="ongole";
+		s1.id=123;
+		s1.amarnath="amarnath";
+	    s1.subbareddy="subbareddy";
+	    s1.getOngole();
+	    s1.getId();
+	    s1.getAmarnath();
+	    s1.getSubbareddy();
+		System.out.println(s1);
+	
+
+	}
+
+}

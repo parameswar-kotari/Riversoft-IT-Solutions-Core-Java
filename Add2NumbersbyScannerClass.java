4e430c34c612a53e8283a9c99135b84efd74a3b7
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-08-24 08:55:30 +0530

    Add files via upload
    added 2 numbers by scanner class

---
+++ Add2NumbersbyScannerClass.java
@@ -0,0 +1,24 @@
+package com.riversoftit.collections;
+import java.util.*;
+
+public class Add2NumbersbyScannerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner readme = new Scanner(System.in);
+		   System.out.println("Enter Two Numbers");
+//		   double n1, n2, n3;
+//		   n1 = readme.nextDouble();
+//		   n2 = readme.nextDouble();
+//		   long n1,n2,n3;
+//		   n1=readme.nextLong();
+//		   n2=readme.nextLong();
+		   float n1,n2,n3;
+		   n1=readme.nextFloat();
+		   n2=readme.nextFloat();
+		   n3 = n1 + n2;
+		   System.out.println("Total = " + n3);
+
+	}
+
+}

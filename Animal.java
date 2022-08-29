9fa5e787bdcdc62f2e63d6cefcb6a5fbed0c0a2a
bnharish12 <110820185+bnharish12@users.noreply.github.com>
2022-08-29 11:54:07 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,36 @@
+package inheritance;
+
+public class Animal {
+
+	 void  eat(){
+		 System.out.println("Eating");
+		
+	 }
+			
+
+}
+class Dog extends Animal {
+
+	void bark()
+	{
+		System.out.println("barking");
+	
+		}
+
+}
+class TestInheritance {
+
+	public static void main(String args[]) {
+
+		Dog d = new Dog();
+
+		d.bark();
+
+		d.eat();
+		
+	}
+
+	}
+
+
+

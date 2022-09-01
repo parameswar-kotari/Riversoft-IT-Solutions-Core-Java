0a4dfd58d92ad1340a0c3f6ad9c8c57d67d5ec8b
Srihari161 <110887029+Srihari161@users.noreply.github.com>
2022-09-01 16:07:32 +0530

    hierarchal inheritance

---
+++ TestInheritance3.java
@@ -0,0 +1,22 @@
+package com.bhavisha;
+class Animal{  
+	void eat(){System.out.println("eating...");}  
+	}  
+	class Dog extends Animal{  
+	void bark(){System.out.println("barking...");}  
+	}  
+	class Cat extends Animal{  
+	void meow(){System.out.println("meowing...");}  
+	}  
+	class TestInheritance3{  
+	public static void main(String args[]){  
+	Cat c=new Cat();  
+	Dog d =new Dog();
+	
+	c.meow();  
+	c.eat(); 
+	d.bark();
+	//c.bark();//C.T.Error  
+	}}
+
+

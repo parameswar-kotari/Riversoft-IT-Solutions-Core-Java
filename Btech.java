d5f00c8bc6a1c98a348ba6c7f407f90f4d0eef69
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-01 15:29:37 +0530

    Hybrid inheretance

---
+++ Btech.java
@@ -0,0 +1,35 @@
+package com.riversoftit.collections;
+
+ class Civil {
+	 void strength() {
+System.out.println("civil engineering");
+}
+ }
+	 class Computer extends Civil {
+		 void python() {
+			 System.out.println("computer engineering");
+		 }
+	 }
+	 class Electric extends Civil{
+		 void electric()
+		 {
+			 System.out.println("elecrical engineering");
+			 
+			 }
+		 }
+	 class Mechanical extends Computer{
+		 void fluid() {
+			 System.out.println("Mecanical engineering");
+		 }
+	 }
+	 public class Btech{
+		 public static void main(String[] args) {
+			 Mechanical obj=new Mechanical();
+			 obj.fluid();
+			 obj.strength();
+			 obj.python();
+		 }
+	 
+		 }
+	 
+	 

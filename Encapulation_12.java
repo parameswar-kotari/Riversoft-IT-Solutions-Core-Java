62d97795dc09fc71d137940f770d745bbcf115cd
Srihari161 <110887029+Srihari161@users.noreply.github.com>
2022-09-01 23:01:50 +0530

    encapulation

---
+++ Encapulation_12.java
@@ -0,0 +1,14 @@
+package com.bhavisha;
+
+public class Encapulation_12 {
+	private String name ;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ Final.java
@@ -0,0 +1,10 @@
+package com.bhavisha;
+
+public class Final {
+	public static void main(String[] args) {
+		Encapulation_12 obj=new Encapulation_12();
+		obj.setName("super");
+		System.out.println(obj.getName());
+	}
+
+}

b3072048d257d747446b2660793885e5cbb2c8a4
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-01 15:40:31 +0530

    hierachal inheritance

---
+++ Bus.java
@@ -0,0 +1,25 @@
+package com.bhavisha;
+
+ class Bus extends Aeroplane {
+
+	
+		public static void main(String args[]) {
+			Aeroplane obj=new Car();
+			 obj.runing();
+		}
+
+		@Override
+		void runing() {
+			// TODO Auto-generated method stu
+			//System.out.println("jahdfugfiuqbfiqgb");
+		}
+ }
+		
+		
+
+	 
+ 
+	
+	
+
+

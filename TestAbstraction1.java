2db4a98bcf756053ad9e8cf85adef53969806d5e
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-01 15:51:02 +0530

    abstaction(overriding)

--- TestAbstraction1.java
+++ TestAbstraction1.java
@@ -1,21 +1,9 @@
-package com.bhavisha;
+  
+//In real scenario, method is called by programmer or user  
+class TestAbstraction1{  
+public static void main(String args[]){  
+Shape2 st=new Shape2();//In a real scenario, object is provided through method, e.g., getShape() method  
+st.draw();
+}  
+}  
 
-abstract class Shape {
-	abstract void draw();
-}
-class Rectangle extends Shape{
-	void draw() {
-		System.out.println("i am bad");
-	}
-}
-class Circle1 extends Shape{
-	void draw() {
-		System.out.println("circle area=pi*radius");
-	}
-}
-class TestAbstraction1{
-	public static void main(String[] args) {
-		Shape s=new Circle1();
-		s.draw();
-	}
-}

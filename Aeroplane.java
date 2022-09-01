6242eb2f502d8d3849607e929af4cdd35e5fa571
Srihari161 <110887029+Srihari161@users.noreply.github.com>
2022-09-01 23:03:10 +0530

    multipel_inheritance

---
+++ Aeroplane.java
@@ -0,0 +1,17 @@
+package com.bhavisha;
+ abstract class Aeroplane {
+	abstract void runing();
+
+}
+ class Bike extends Aeroplane{
+	 void runing() {
+	
+	 System.out.println("jet is fast");
+	 }
+ }
+ class Car extends Aeroplane{
+	 void runing() {
+		 System.out.println("car is  veryfast");
+ }
+ }
+  

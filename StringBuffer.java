f9d128e196aad20bc4a561fc449bbb7f4d90ed6e
Venkatsai19hm <110887633+Venkatsai19hm@users.noreply.github.com>
2022-09-07 14:44:39 +0530

    StringBufferBuilder

---
+++ StringBuffer.java
@@ -0,0 +1,13 @@
+package com.riversoftit.collections;
+
+public class StringBuffer {
+	public static void main(String[] args) {
+		
+		String str=new String("tata");
+		System.out.println(str.hashCode());
+		System.out.println(str);
+		
+	}
+	
+
+}

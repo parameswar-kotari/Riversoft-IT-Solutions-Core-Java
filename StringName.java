8df2cd4d12358dd5528095a23befdc349ebd294b
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-07 15:02:16 +0530

    StringBuffer

---
+++ StringName.java
@@ -0,0 +1,25 @@
+package com.riversoftit.collections;
+
+public class StringName {
+	public static void main(String[] args) {
+		
+		String str=new String("tata");
+		System.out.println(str.hashCode());
+		System.out.println(str);
+		String str2;
+		String str21=new String("byebye");
+		System.out.println(str21);
+		System.out.println(str21.hashCode());
+		StringBuilder s4=new StringBuilder("happy");
+		System.out.println(s4.reverse());
+		StringBuilder s6=new StringBuilder("Super");
+		System.out.println(s6);
+		StringBuffer uhd=new StringBuffer("sunday");
+		System.out.println(uhd);
+		System.out.println(uhd.reverse());
+		
+		
+	}
+	
+
+}

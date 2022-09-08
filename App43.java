5f915fc0db287cf57e0961cf68faf65b51e5fc1b
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-08 23:05:57 +0530

    encapulation using getter and  setter

---
+++ App43.java
@@ -0,0 +1,35 @@
+package com.modfiers;
+
+public class App43 {
+private int age;
+private String name;
+private String place;
+public int getAge() {
+	return age;
+}
+public String getName() {
+	return name;
+}
+public String getPlace() {
+	return place;
+}
+public int getNumber() {
+	return number;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void setPlace(String place) {
+	this.place = place;
+}
+public void setNumber(int number) {
+	this.number = number;
+}
+private int number;
+
+
+
+}
---
+++ App44.java
@@ -0,0 +1,16 @@
+package com.modfiers;
+
+public class App44 {
+	public static void main(String[] args) {
+		App43 obj=new App43();
+		obj.setAge(653);
+		obj.setName("apple");
+		obj.setNumber(785);
+		obj.setPlace("hydrabad");
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		System.out.println(obj.getNumber());
+		System.out.println(obj.getPlace());
+	}
+
+}

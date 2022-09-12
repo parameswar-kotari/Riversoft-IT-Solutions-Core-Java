6ea3fe569c32beb32ccd3a9d71b8e780096f4f72
pooja4800 <poojam4800@gmail.com>
2022-09-12 15:18:32 +0530

    the programme is running

---
+++ core-java-sessions/src/com/riversoftit/collections/SetInterface.java
@@ -0,0 +1,17 @@
+package com.riversoftit.collections;
+
+import java.util.HashSet;
+
+public class SetInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet fd=new HashSet();
+		fd.add("mass");
+		fd.add("varsham");
+		fd.add("mass");
+		System.out.println(fd);
+
+	}
+
+}
---
+++ core-java-sessions/src/com/riversoftit/collections/Setdemo.java
@@ -0,0 +1,17 @@
+package com.riversoftit.collections;
+
+import java.util.HashSet;
+
+public class Setdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> fd=new HashSet<String>();
+		fd.add("mass");
+		fd.add("varsham");
+		fd.add("mass");
+		System.out.println(fd);
+
+	}
+
+}

7525b73ec5cd791fd1791461cd754e95770e4536
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-13 11:49:20 +0530

    Array List

---
+++ ListDemo.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		//List obj= new ArrayList();
+		//obj.add(34);
+		//obj.add("goat");
+		//obj.add(1, "hy");
+		
+		List obj1=new ArrayList();
+		obj1.add("ab");
+		obj1.add("yhr");
+		obj1.add("hyg");
+		//obj1.addAll(obj1 );
+		Collections.sort(obj1);
+		System.out.println(obj1);
+	
+		//for(int i=0;i<obj.size();i++) {
+			//System.out.println(obj.get(i));
+		//}
+		
+		//System.out.println(obj.);
+		//Iterator obj45=obj1.iterator();
+		//while(obj45.hasNext()){
+		//	System.out.println(obj45.next());
+			
+//		}
+	}
+
+}

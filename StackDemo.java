6831bff8d0ec6eaec9476e9889ebc18f77497d5c
bhavishya-vontimitta <110707844+bhavishya-vontimitta@users.noreply.github.com>
2022-09-13 13:07:50 +0530

    stack

---
+++ StackDemo.java
@@ -0,0 +1,23 @@
+package com.bhavisha;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		Stack <Integer>obj=new Stack();
+		obj.push(3534);
+		obj.push(553);
+		obj.push(55566);
+		obj.pop();
+		obj.pop();
+		obj.toString();
+		//System.out.println(obj.peek());
+		System.out.println(obj.toString());
+			
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
